b4fe2b3c86ec3ed2b935a94bceddcc5f658df7c5
Janeen <janeen.e.scott@gmail.com>
2019-03-14 22:30:42 -0400

    removed unnecessary fields in profile template

--- buddies/urls.py
+++ buddies/urls.py
@@ -9,7 +9,4 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
     path('chat/', views.ChatView.as_view(), name='chat'),
-
-    # path('users/', views.UsersInSystemView.as_view(), name='users'),
-    # path('user-equipment/<int:pk>/', views.UserEquipmentList.as_view(), name='user-equipment'),
 ]
