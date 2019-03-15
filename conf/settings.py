c3214211a66ea6184394df47a4af5595b4d905a3
Janeen <janeen.e.scott@gmail.com>
2019-03-15 14:33:49 -0400

    added static files dir for react in heroku

--- conf/settings.py
+++ conf/settings.py
@@ -29,6 +29,9 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'frontend/static/build/static'),
+)
 # ======== END STATIC FILE CONFIGURATION
 
 # Quick-start development settings - unsuitable for production
