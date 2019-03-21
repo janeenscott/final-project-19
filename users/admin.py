c8a9cb5f129dba0913611b189a4f8f711991c5a4
Janeen <janeen.e.scott@gmail.com>
2019-03-21 10:51:05 -0400

    added pair buddies action in admin, need to write method for actual pairing

--- users/admin.py
+++ users/admin.py
@@ -6,11 +6,23 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
 
+def pair_buddies(self, request, queryset):
+    queryset.update(buddy=fk)
+    # this is not right, I need to figure out how to actually write the queryset
+
+
+pair_buddies.short_description = "Pair Buddies"
+
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', 'age', 'is_staff', 'city', 'image',]
+    list_display = ['email', 'username', 'age', 'is_staff', 'city', 'image', 'buddy']
+    actions = [pair_buddies]
+
+
+
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
