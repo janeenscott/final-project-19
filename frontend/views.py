3357d3ed4351ad222a899e856e78e239b9042fff
Janeen <janeen.e.scott@gmail.com>
2019-03-26 19:41:01 -0400

    requires user to be logged in to see chat screen

--- frontend/views.py
+++ frontend/views.py
@@ -2,9 +2,10 @@ import os
 from django.views import View
 from django.http import HttpResponse
 from django.conf import settings
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ChatView(View):
+class ChatView(LoginRequiredMixin, View):
     def get(self, request):
         try:
             with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
@@ -13,3 +14,7 @@ class ChatView(View):
             return HttpResponse(status=501)
 
 
+
+    # def determine_if_paired(self):
+    #     # user is not allowed to see page if buddy__isnull
+    #     pass
