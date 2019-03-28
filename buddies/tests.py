f6358c90f0bb90bc5ab52a94007494db2a6fbd12
Janeen <janeen.e.scott@gmail.com>
2019-03-27 23:48:59 -0400

    wrote tests for buddies routes

--- buddies/tests.py
+++ buddies/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class BuddiesTests(TestCase):
+
+    def buddies_index(self):
+        view_url = reverse('buddies:index')
+        response = self.client.get(view_url)
+        self.assertEqual(response.status_code, 200)
+
+    def buddies_profile(self):
+        view_url = reverse('buddies:profile')
+        response = self.client.get(view_url)
+        self.assertEqual(response.status_code, 200)
